67fbf7025243287c72c98780c361a6351ac68e96
Praseedha Praveen Kalbhavi <praseedhakalbhavi@gmail.com>
2022-01-31 16:07:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -72,29 +72,6 @@ def response(user_response):
 # In[6]:
 
 
-def text_to_speech(text, gender):
-    voice_dict = {'Male': 0, 'Female': 1}
-    code = voice_dict[gender]
-
-    engine = pyttsx3.init()
-
-    # Setting up voice rate
-    engine.setProperty('rate', 125)
-
-    # Setting up volume level  between 0 and 1
-    engine.setProperty('volume', 0.8)
-
-    # Change voices: 0 for male and 1 for female
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[code].id)
-
-    engine.say(text)
-    engine.runAndWait()
-
-
-# In[7]:
-
-
 app = Flask(__name__)
 
 #define app routes
